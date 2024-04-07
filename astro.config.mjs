4c2dec86139e064387fa93aed4972c2ceee103f5
PaulHaze <dev@artemist.com.au>
2024-04-08 09:01:13 +1000

    add cloudflare SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare';
 
-import icon from "astro-icon";
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), react(), icon()]
+	output: 'server',
+	adapter: cloudflare(),
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		react(),
+		icon(),
+	],
 });
