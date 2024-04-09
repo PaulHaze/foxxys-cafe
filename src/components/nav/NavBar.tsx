e8521a264edba861c8eef1a04a616cc535d2bee9
PaulHaze <dev@artemist.com.au>
2024-04-10 09:10:29 +1000

    add basic react navbar

---
+++ src/components/nav/NavBar.tsx
@@ -0,0 +1,9 @@
+export function NavBar() {
+	return (
+		<nav className="mx-auto flex h-[64px] w-full max-w-lg items-center justify-between px-6 text-white">
+			<div className="">LOGO</div>
+
+			<div className="">NAV LINKS</div>
+		</nav>
+	);
+}
