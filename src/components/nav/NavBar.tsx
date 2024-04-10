6317e2def202c5ad2cfaf99dc7f8eb4b258cb24f
PaulHaze <dev@artemist.com.au>
2024-04-10 11:05:36 +1000

    add scroll to sections on nav click

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -3,7 +3,17 @@ export function NavBar() {
 		<nav className="mx-auto flex h-[64px] w-full max-w-lg items-center justify-between px-6 text-white">
 			<div className="">LOGO</div>
 
-			<div className="">NAV LINKS</div>
+			<ul className="flex gap-x-4">
+				<li>
+					<a href="#about">About</a>
+				</li>
+				<li>
+					<a href="#menu">Menu</a>
+				</li>
+				<li>
+					<a href="#contact">Contact</a>
+				</li>
+			</ul>
 		</nav>
 	);
 }
