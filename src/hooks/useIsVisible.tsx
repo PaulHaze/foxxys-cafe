25df988937dc906f7a0a4235f3fc3d05b4e36ce7
PaulHaze <dev@artemist.com.au>
2024-05-27 10:56:52 +1000

    fix ts errors in useIsVisible hook

--- src/hooks/useIsVisible.tsx
+++ src/hooks/useIsVisible.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+
 // import type { RefObject } from 'react';
 
 // interface RefObject<T> {
@@ -6,10 +7,12 @@ import { useEffect, useState } from 'react';
 // 	readonly current: T | null;
 // }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function useIsVisible(ref: any) {
 	const [isIntersecting, setIntersecting] = useState(false);
 
 	useEffect(() => {
+		// eslint-disable-next-line no-undef
 		const observer = new IntersectionObserver(([entry]) =>
 			setIntersecting(entry.isIntersecting)
 		);
